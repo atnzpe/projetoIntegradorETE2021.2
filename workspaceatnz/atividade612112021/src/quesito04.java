6c834de72231c96027212a93e30aa62afa7ad528
Gleyson Atanazio <gleysonasilva@gmail.com>
2021-11-27 18:06:56 -0300

    cria estrutura da biblioteca Scanner

--- workspaceatnz/atividade612112021/src/quesito04.java
+++ workspaceatnz/atividade612112021/src/quesito04.java
@@ -2,15 +2,15 @@
  * 4. Calcular e apresentar o valor do volume de uma lata de óleo, utilizando fórmula: 
  * V = 3.14159 * R * R * A, 
  * em que as variáveis: V, R e A representam respectivamente o volume, o raio e a altura.
-
  */
 
 /**
  *
  * @author gleyson
  */
+import java.util.Scanner;
 
 public class quesito04 {
-    
+    Scanner input = new Scanner(System.in);
     
 }
